0cd6fc15a18bda99bffd04072ed6a202618994ce
Heather Floyd <heatherflo@gmail.com>
2023-12-20 14:42:33 -0700

    environment set up

--- client/app/env.js
+++ client/app/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'dev-z4hmit7z70rm51po.us.auth0.com'
+export const audience = 'https://SweetSummer.com'
+export const clientId = 'OxuBQY8sEe15P1c2xINRGK8QUi5E4pGG'
